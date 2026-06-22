b251bbdbc38c413bef235bf9e8c69571f1e54270
Mugesh work <mukeshm50266@gmail.com>
2026-06-23 00:34:45 +0530

    1st commit

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Database Settings
-    DATABASE_URL: str = "mysql+pymysql://root:root@localhost:3306/gold_billing"
+    DATABASE_URL: str = "postgresql://gold_billing_app_db_user:pvN28IsTXmhOymBXZ4asITOVmW99W0Sr@dpg-d8slglu8bjmc738hjg0g-a:5432/gold_billing_app_db"
     
     # Security Settings
     SECRET_KEY: str = "luxury_gold_showroom_secret_key_2026_safe_and_long"
